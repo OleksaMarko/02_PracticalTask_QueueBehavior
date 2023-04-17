e00d41e84bb8a41917012688d44134a3f31b8e99
OleksaMarko <kompotius43@gmail.com>
2023-04-17 16:56:33 +0300

    fixed disable button fn

--- README.md
+++ README.md
@@ -6,25 +6,26 @@
 
 This is queue behavior app, test task for "TM"
 
-
 ### Installing
 
 If you have Git installed on your machine, you can clone the repository:
-* Clone the repository: git clone https://github.com/OleksaMarko/02_PracticalTask_QueueBehavior.git
+
+- Clone the repository: git clone https://github.com/OleksaMarko/02_PracticalTask_QueueBehavior.git
 
 Or here are the instructions for downloading a project from GitHub:
-* Go to the GitHub repository - https://github.com/OleksaMarko/02_PracticalTask_QueueBehavior.git
-* Click on the green "Code" button on the right side of the page.
-* Click on the "Download ZIP" option. This will download the entire repository as a ZIP file.
-* Extract the ZIP file to a location of your choice.
-* Navigate to the extracted directory on your local machine.
-* You should now be able to access the project files on your local machine. 
-* Run index.html file on your local machine.
+
+- Go to the GitHub repository - https://github.com/OleksaMarko/02_PracticalTask_QueueBehavior.git
+- Click on the green "Code" button on the right side of the page.
+- Click on the "Download ZIP" option. This will download the entire repository as a ZIP file.
+- Extract the ZIP file to a location of your choice.
+- Navigate to the extracted directory on your local machine.
+- You should now be able to access the project files on your local machine.
+- Run index.html file on your local machine.
 
 ### Executing program
 
-* Open index.html in your web browser.
-* You're ready to go!
+- Open index.html in your web browser.
+- You're ready to go!
 
 ## Help
 
@@ -37,5 +38,5 @@ ex. kompotius43@gmail.com
 
 ## Version History
 
-* 0.1
-    * Initial Release
+- 0.1
+  - Initial Release
--- index.js
+++ index.js
@@ -2,85 +2,84 @@
 
 const myAge = 32;
 
-const storage = localStorage.getItem('queue');
+const storage = localStorage.getItem("queue");
 
 let queue = [];
 
 let queueList = document.getElementById("queue-list");
 
 if (JSON.parse(storage).length !== 0) {
-    queue = JSON.parse(storage)
-    renderQueue(queue)
+  queue = JSON.parse(storage);
+  renderQueue(queue);
 }
 
 function renderQueue(list) {
-    queueList.innerHTML = '';
+  queueList.innerHTML = "";
 
-    for (let i = 0; i < list.length; i++) {
-        let newListItem = document.createElement('li')
-        newListItem.innerHTML = list[i]
-        queueList.appendChild(newListItem)
-    }
+  for (let i = 0; i < list.length; i++) {
+    let newListItem = document.createElement("li");
+    newListItem.innerHTML = list[i];
+    queueList.appendChild(newListItem);
+  }
 }
 
 function addQueueItem(item, arr) {
-    if (arr.length > myAge - 1) {
-        return alert('Your queue is too loong')
-    } else {
-        return queue.push(item)
-    }
+  if (arr.length > myAge - 1) {
+    return alert("Your queue is too loong");
+  } else {
+    return queue.push(item);
+  }
 }
 
 function addToLocalStorage(arr) {
-    localStorage.clear()
-    localStorage.setItem('queue', JSON.stringify(arr))
+  localStorage.clear();
+  localStorage.setItem("queue", JSON.stringify(arr));
 }
 
 function deleteQueueItem(arr) {
-    if (queue.length === 0) {
-        alert("There are nothing left in your queue")
-    }
-    return arr.shift()
+  if (queue.length === 0) {
+    alert("There are nothing left in your queue");
+  }
+  return arr.shift();
 }
 
 function deleteFromLocalStorage(arr) {
-    localStorage.clear()
-    localStorage.setItem('queue', JSON.stringify(arr))
+  localStorage.clear();
+  localStorage.setItem("queue", JSON.stringify(arr));
 }
 
 if (queue.length < 1) {
-    queueList.innerHTML = "Your  queue list is empty"
+  queueList.innerHTML = "Your  queue list is empty";
 }
 
 let inputForm = document.getElementById("form");
-let submitButton = document.getElementById('submit')
+let submitButton = document.getElementById("submit");
 let inputValue = document.getElementById("input");
 
 submitButton.disabled = true;
-inputValue.addEventListener("input", (e) => submitButton.disabled = false)
+inputValue.addEventListener("input", (e) => {
+  submitButton.disabled = !e.target.value;
+});
 inputForm.addEventListener("submit", (e) => {
-    e.preventDefault();
+  e.preventDefault();
 
-    if (inputValue.value == "") {
-        alert("Ensure your input is correct");
-    } else {
-        console.log(`This form has a input - ${inputValue.value}`);
-        addQueueItem(inputValue.value, queue);
-        renderQueue(queue)
-        addToLocalStorage(queue)
-        submitButton.disabled = true;
-        inputValue.value = "";
-    }
+  if (inputValue.value == "") {
+    alert("Ensure your input is correct");
+  } else {
+    console.log(`This form has a input - ${inputValue.value}`);
+    addQueueItem(inputValue.value, queue);
+    renderQueue(queue);
+    addToLocalStorage(queue);
+    submitButton.disabled = true;
+    inputValue.value = "";
+  }
 });
 
-const deleteButton = document.getElementById('deleteButton');
+const deleteButton = document.getElementById("deleteButton");
 
-deleteButton.addEventListener('click', (e) => {
-    deleteQueueItem(queue)
-    renderQueue(queue)
-    deleteFromLocalStorage(queue)
-    inputValue.value = "";
-})
-
-
-
+deleteButton.addEventListener("click", (e) => {
+  deleteQueueItem(queue);
+  renderQueue(queue);
+  deleteFromLocalStorage(queue);
+  inputValue.value = "";
+});
